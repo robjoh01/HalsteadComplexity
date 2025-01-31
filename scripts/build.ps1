be0374deb9a8fa8b54d39cc23d083660498aaac8
Albin Carlsson <github.garter593@passmail.net>
2025-01-31 18:16:39 +0100

    readchar dep + default icon build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,7 +2,8 @@
 & .\.venv\Scripts\Activate.ps1
 
 # 2. Run PyInstaller to create the .exe file with the required dependencies
-pyinstaller --onefile --clean --name HalsteadComplexity --icon=img\icon.ico --add-data "requirements.txt;." --copy-metadata readchar src\main.py
+# pyinstaller --onefile --clean --name HalsteadComplexity --icon=img\icon.ico --add-data "requirements.txt;." --copy-metadata readchar src\main.py
+pyinstaller --onefile --clean --name HalsteadComplexity --add-data "requirements.txt;." --copy-metadata readchar src\main.py
 
 # 3. Deactivate the virtual environment
 deactivate
